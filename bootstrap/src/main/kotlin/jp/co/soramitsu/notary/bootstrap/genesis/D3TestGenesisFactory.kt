d662134fad1422c3ed21088df8e3f598d52ecf18
iurii.vinogradov <discours@list.ru>
2019-02-27 09:39:08 +0300

    - bootstrap service: Refactoring of genesis block creation

--- bootstrap/src/main/kotlin/jp/co/soramitsu/notary/bootstrap/genesis/D3TestGenesisFactory.kt
+++ bootstrap/src/main/kotlin/jp/co/soramitsu/notary/bootstrap/genesis/D3TestGenesisFactory.kt
@@ -2,6 +2,7 @@ package jp.co.soramitsu.notary.bootstrap.genesis
 
 import com.google.protobuf.util.JsonFormat
 import jp.co.soramitsu.iroha.java.Transaction
+import jp.co.soramitsu.iroha.java.TransactionBuilder
 import jp.co.soramitsu.iroha.testcontainers.detail.GenesisBlockBuilder
 import jp.co.soramitsu.notary.bootstrap.dto.IrohaAccountDto
 import jp.co.soramitsu.notary.bootstrap.dto.Peer
@@ -17,30 +18,93 @@ class D3TestGenesisFactory : GenesisInterface {
     }
 
     override fun createGenesisBlock(accounts: List<IrohaAccountDto>, peers: List<Peer>, blockVersion: String): String {
-        val builder = GenesisBlockBuilder()
-        
-        createPeers(peers, builder)
-        createRoles(builder)
+        val transactionBuilder = Transaction.builder(null)
 
-        val block = builder.build()
+        createPeers(peers, transactionBuilder)
+        createRoles(transactionBuilder)
+
+        val blockBuilder = GenesisBlockBuilder().addTransaction(transactionBuilder.build().build())
+        val block = blockBuilder.build()
         return JsonFormat.printer().print(block)
     }
 
+    /*fun createDomain()*/
+
     private fun createPeers(
         peers: List<Peer>,
-        builder: GenesisBlockBuilder
+        builder: TransactionBuilder
     ) {
         peers.forEach {
-            builder.addTransaction(
-                Transaction.builder(null)
-                    .addPeer(it.hostPort, getIrohaPublicKeyFromHexString(it.peerKey))
-                    .build() // returns ipj model Transaction
-                    .build()
-            )
+            builder
+                .addPeer(it.hostPort, getIrohaPublicKeyFromHexString(it.peerKey))
         }
     }
 
-    private fun createRoles(builder: GenesisBlockBuilder) {
+    /*  {
+          "createDomain": {
+          "domainId": "notary",
+          "defaultRole": "none"
+      }
+      },
+      {
+          "createDomain": {
+          "domainId": "d3",
+          "defaultRole": "client"
+      }
+      },
+      {
+          "createDomain": {
+          "domainId": "btcSession",
+          "defaultRole": "none"
+      }
+      },
+      {
+          "createDomain": {
+          "domainId": "ethereum",
+          "defaultRole": "none"
+      }
+      },
+      {
+          "createDomain": {
+          "domainId": "sora",
+          "defaultRole": "sora_client"
+      }
+      },
+      {
+          "createAsset": {
+          "assetName": "xor",
+          "domainId": "sora",
+          "precision": 0
+      }
+      },
+      {
+          "createAsset": {
+          "assetName": "ether",
+          "domainId": "ethereum",
+          "precision": 18
+      }
+      },
+      {
+          "createDomain": {
+          "domainId": "bitcoin",
+          "defaultRole": "none"
+      }
+      },
+      {
+          "createDomain": {
+          "domainId": "btcSignCollect",
+          "defaultRole": "none"
+      }
+      },
+      {
+          "createAsset": {
+          "assetName": "btc",
+          "domainId": "bitcoin",
+          "precision": 8
+      }
+      },*/
+
+    private fun createRoles(builder: TransactionBuilder) {
         createNotaryRole(builder)
         createRelayDeployerRole(builder)
         createEthTokenListStorageRole(builder)
--- bootstrap/src/main/kotlin/jp/co/soramitsu/notary/bootstrap/genesis/D3Utils.kt
+++ bootstrap/src/main/kotlin/jp/co/soramitsu/notary/bootstrap/genesis/D3Utils.kt
@@ -2,308 +2,232 @@ package jp.co.soramitsu.notary.bootstrap.genesis
 
 import iroha.protocol.Primitive
 import jp.co.soramitsu.iroha.java.Transaction
+import jp.co.soramitsu.iroha.java.TransactionBuilder
 import jp.co.soramitsu.iroha.testcontainers.detail.GenesisBlockBuilder
 
 
-fun createNotaryRole(builder: GenesisBlockBuilder) {
-    builder.addTransaction(
-        Transaction.builder(null)
-            // create default "user" role
-            .createRole(
-                "notary",
-                listOf(
-                    Primitive.RolePermission.can_get_all_acc_ast,
-                    Primitive.RolePermission.can_get_all_accounts,
-                    Primitive.RolePermission.can_create_asset,
-                    Primitive.RolePermission.can_add_asset_qty,
-                    Primitive.RolePermission.can_transfer,
-                    Primitive.RolePermission.can_set_detail,
-                    Primitive.RolePermission.can_get_all_txs,
-                    Primitive.RolePermission.can_receive,
-                    Primitive.RolePermission.can_get_blocks,
-                    Primitive.RolePermission.can_read_assets,
-                    Primitive.RolePermission.can_add_signatory,
-                    Primitive.RolePermission.can_set_quorum,
-                    Primitive.RolePermission.can_grant_can_set_my_quorum,
-                    Primitive.RolePermission.can_grant_can_add_my_signatory,
-                    Primitive.RolePermission.can_grant_can_transfer_my_assets
-                )
-            ).build()
-            .build()
+fun createNotaryRole(builder: TransactionBuilder) {
+    builder.createRole(
+        "notary",
+        listOf(
+            Primitive.RolePermission.can_get_all_acc_ast,
+            Primitive.RolePermission.can_get_all_accounts,
+            Primitive.RolePermission.can_create_asset,
+            Primitive.RolePermission.can_add_asset_qty,
+            Primitive.RolePermission.can_transfer,
+            Primitive.RolePermission.can_set_detail,
+            Primitive.RolePermission.can_get_all_txs,
+            Primitive.RolePermission.can_receive,
+            Primitive.RolePermission.can_get_blocks,
+            Primitive.RolePermission.can_read_assets,
+            Primitive.RolePermission.can_add_signatory,
+            Primitive.RolePermission.can_set_quorum,
+            Primitive.RolePermission.can_grant_can_set_my_quorum,
+            Primitive.RolePermission.can_grant_can_add_my_signatory,
+            Primitive.RolePermission.can_grant_can_transfer_my_assets
+        )
     )
 }
 
-fun createRelayDeployerRole(builder:GenesisBlockBuilder) {
-    builder.addTransaction(
-        Transaction.builder(null)
-            // create default "user" role
-            .createRole(
-                "relay_deployer",
-                listOf(
-                    Primitive.RolePermission.can_set_detail,
-                    Primitive.RolePermission.can_get_domain_accounts
-                )
-            ).build()
-            .build()
+fun createRelayDeployerRole(builder: TransactionBuilder) {
+    builder.createRole(
+        "relay_deployer",
+        listOf(
+            Primitive.RolePermission.can_set_detail,
+            Primitive.RolePermission.can_get_domain_accounts
+        )
     )
 }
 
-fun createEthTokenListStorageRole(builder:GenesisBlockBuilder) {
-    builder.addTransaction(
-        Transaction.builder(null)
-            // create default "user" role
-            .createRole(
-                "eth_token_list_storage",
-                listOf(
-                    Primitive.RolePermission.can_set_detail,
-                    Primitive.RolePermission.can_create_asset
-                )
-            ).build()
-            .build()
+fun createEthTokenListStorageRole(builder: TransactionBuilder) {
+    builder.createRole(
+        "eth_token_list_storage",
+        listOf(
+            Primitive.RolePermission.can_set_detail,
+            Primitive.RolePermission.can_create_asset
+        )
     )
 }
 
-fun createRegistrationServiceRole(builder:GenesisBlockBuilder) {
-    builder.addTransaction(
-        Transaction.builder(null)
-            // create default "user" role
-            .createRole(
-                "registration_service",
-                listOf(
-                    Primitive.RolePermission.can_create_account,
-                    Primitive.RolePermission.can_append_role,
-                    Primitive.RolePermission.can_set_detail,
-                    Primitive.RolePermission.can_get_all_accounts,
-                    Primitive.RolePermission.can_get_domain_accounts,
-                    Primitive.RolePermission.can_get_all_txs,
-                    Primitive.RolePermission.can_get_blocks,
-                    Primitive.RolePermission.can_set_quorum
-                )
-            ).build()
-            .build()
+fun createRegistrationServiceRole(builder: TransactionBuilder) {
+    builder.createRole(
+        "registration_service",
+        listOf(
+            Primitive.RolePermission.can_create_account,
+            Primitive.RolePermission.can_append_role,
+            Primitive.RolePermission.can_set_detail,
+            Primitive.RolePermission.can_get_all_accounts,
+            Primitive.RolePermission.can_get_domain_accounts,
+            Primitive.RolePermission.can_get_all_txs,
+            Primitive.RolePermission.can_get_blocks,
+            Primitive.RolePermission.can_set_quorum
+        )
     )
 }
 
-fun createClientRole(builder:GenesisBlockBuilder) {
-    builder.addTransaction(
-        Transaction.builder(null)
-            // create default "user" role
-            .createRole(
-                "client",
-                listOf(
-                    Primitive.RolePermission.can_get_my_account,
-                    Primitive.RolePermission.can_get_my_acc_ast,
-                    Primitive.RolePermission.can_get_my_acc_ast_txs,
-                    Primitive.RolePermission.can_get_my_acc_txs,
-                    Primitive.RolePermission.can_get_my_txs,
-                    Primitive.RolePermission.can_transfer,
-                    Primitive.RolePermission.can_receive,
-                    Primitive.RolePermission.can_set_quorum,
-                    Primitive.RolePermission.can_add_signatory,
-                    Primitive.RolePermission.can_get_my_signatories,
-                    Primitive.RolePermission.can_remove_signatory
-                )
-            ).build()
-            .build()
+fun createClientRole(builder: TransactionBuilder) {
+    builder.createRole(
+        "client",
+        listOf(
+            Primitive.RolePermission.can_get_my_account,
+            Primitive.RolePermission.can_get_my_acc_ast,
+            Primitive.RolePermission.can_get_my_acc_ast_txs,
+            Primitive.RolePermission.can_get_my_acc_txs,
+            Primitive.RolePermission.can_get_my_txs,
+            Primitive.RolePermission.can_transfer,
+            Primitive.RolePermission.can_receive,
+            Primitive.RolePermission.can_set_quorum,
+            Primitive.RolePermission.can_add_signatory,
+            Primitive.RolePermission.can_get_my_signatories,
+            Primitive.RolePermission.can_remove_signatory
+        )
     )
 }
 
-fun createWithdrawalRole(builder:GenesisBlockBuilder) {
-    builder.addTransaction(
-        Transaction.builder(null)
-            // create default "user" role
-            .createRole(
-                "withdrawal",
-                listOf(
-                    Primitive.RolePermission.can_get_all_accounts,
-                    Primitive.RolePermission.can_get_blocks,
-                    Primitive.RolePermission.can_read_assets,
-                    Primitive.RolePermission.can_receive,
-                    Primitive.RolePermission.can_get_all_txs
-                )
-            ).build()
-            .build()
+fun createWithdrawalRole(builder: TransactionBuilder) {
+    builder.createRole(
+        "withdrawal",
+        listOf(
+            Primitive.RolePermission.can_get_all_accounts,
+            Primitive.RolePermission.can_get_blocks,
+            Primitive.RolePermission.can_read_assets,
+            Primitive.RolePermission.can_receive,
+            Primitive.RolePermission.can_get_all_txs
+        )
+
     )
 }
 
-fun createSignatureCollectorRole(builder:GenesisBlockBuilder) {
-    builder.addTransaction(
-        Transaction.builder(null)
-            // create default "user" role
-            .createRole(
-                "signature_collector",
-                listOf(
-                    Primitive.RolePermission.can_create_account,
-                    Primitive.RolePermission.can_set_detail,
-                    Primitive.RolePermission.can_get_all_accounts
-                )
-            ).build()
-            .build()
+fun createSignatureCollectorRole(builder: TransactionBuilder) {
+    builder.createRole(
+        "signature_collector",
+        listOf(
+            Primitive.RolePermission.can_create_account,
+            Primitive.RolePermission.can_set_detail,
+            Primitive.RolePermission.can_get_all_accounts
+        )
+
     )
 }
 
-fun createVacuumerRole(builder:GenesisBlockBuilder) {
-    builder.addTransaction(
-        Transaction.builder(null)
-            // create default "user" role
-            .createRole(
-                "Vacuumer",
-                listOf(
-                    Primitive.RolePermission.can_get_domain_accounts,
-                    Primitive.RolePermission.can_read_assets
-                )
-            ).build()
-            .build()
+fun createVacuumerRole(builder: TransactionBuilder) {
+    builder.createRole(
+        "Vacuumer",
+        listOf(
+            Primitive.RolePermission.can_get_domain_accounts,
+            Primitive.RolePermission.can_read_assets
+        )
+
     )
 }
 
-fun createNoneRole(builder:GenesisBlockBuilder) {
-    builder.addTransaction(
-        Transaction.builder(null)
-            // create default "user" role
-            .createRole(
-                "none",
-                listOf()
-            ).build()
-            .build()
+fun createNoneRole(builder: TransactionBuilder) {
+    builder.createRole(
+        "none",
+        listOf()
     )
 }
 
-fun createTesterRole(builder:GenesisBlockBuilder) {
-    builder.addTransaction(
-        Transaction.builder(null)
-            // create default "user" role
-            .createRole(
-                "tester",
-                listOf(
-                    Primitive.RolePermission.can_create_account,
-                    Primitive.RolePermission.can_set_detail,
-                    Primitive.RolePermission.can_create_asset,
-                    Primitive.RolePermission.can_transfer,
-                    Primitive.RolePermission.can_receive,
-                    Primitive.RolePermission.can_add_asset_qty,
-                    Primitive.RolePermission.can_subtract_asset_qty,
-                    Primitive.RolePermission.can_create_domain,
-                    Primitive.RolePermission.can_grant_can_add_my_signatory,
-                    Primitive.RolePermission.can_grant_can_remove_my_signatory,
-                    Primitive.RolePermission.can_grant_can_set_my_quorum,
-                    Primitive.RolePermission.can_grant_can_transfer_my_assets,
-                    Primitive.RolePermission.can_add_peer,
-                    Primitive.RolePermission.can_append_role,
-                    Primitive.RolePermission.can_create_role,
-                    Primitive.RolePermission.can_detach_role,
-                    Primitive.RolePermission.can_add_signatory,
-                    Primitive.RolePermission.can_remove_signatory,
-                    Primitive.RolePermission.can_set_quorum,
-                    Primitive.RolePermission.can_get_all_acc_detail,
-                    Primitive.RolePermission.can_get_all_accounts,
-                    Primitive.RolePermission.can_get_all_acc_ast,
-                    Primitive.RolePermission.can_get_blocks,
-                    Primitive.RolePermission.can_get_roles,
-                    Primitive.RolePermission.can_get_all_signatories,
-                    Primitive.RolePermission.can_get_domain_accounts,
-                    Primitive.RolePermission.can_get_all_txs,
-                    Primitive.RolePermission.can_get_domain_acc_detail,
-                    Primitive.RolePermission.can_read_assets
-                )
-            ).build()
-            .build()
+fun createTesterRole(builder: TransactionBuilder) {
+    builder.createRole(
+        "tester",
+        listOf(
+            Primitive.RolePermission.can_create_account,
+            Primitive.RolePermission.can_set_detail,
+            Primitive.RolePermission.can_create_asset,
+            Primitive.RolePermission.can_transfer,
+            Primitive.RolePermission.can_receive,
+            Primitive.RolePermission.can_add_asset_qty,
+            Primitive.RolePermission.can_subtract_asset_qty,
+            Primitive.RolePermission.can_create_domain,
+            Primitive.RolePermission.can_grant_can_add_my_signatory,
+            Primitive.RolePermission.can_grant_can_remove_my_signatory,
+            Primitive.RolePermission.can_grant_can_set_my_quorum,
+            Primitive.RolePermission.can_grant_can_transfer_my_assets,
+            Primitive.RolePermission.can_add_peer,
+            Primitive.RolePermission.can_append_role,
+            Primitive.RolePermission.can_create_role,
+            Primitive.RolePermission.can_detach_role,
+            Primitive.RolePermission.can_add_signatory,
+            Primitive.RolePermission.can_remove_signatory,
+            Primitive.RolePermission.can_set_quorum,
+            Primitive.RolePermission.can_get_all_acc_detail,
+            Primitive.RolePermission.can_get_all_accounts,
+            Primitive.RolePermission.can_get_all_acc_ast,
+            Primitive.RolePermission.can_get_blocks,
+            Primitive.RolePermission.can_get_roles,
+            Primitive.RolePermission.can_get_all_signatories,
+            Primitive.RolePermission.can_get_domain_accounts,
+            Primitive.RolePermission.can_get_all_txs,
+            Primitive.RolePermission.can_get_domain_acc_detail,
+            Primitive.RolePermission.can_read_assets
+        )
     )
 }
 
-fun createWhiteListSetterRole(builder:GenesisBlockBuilder) {
-    builder.addTransaction(
-        Transaction.builder(null)
-            // create default "user" role
-            .createRole(
-                "whitelist_setter",
-                listOf(
-                    Primitive.RolePermission.can_set_detail
-                )
-            ).build()
-            .build()
+fun createWhiteListSetterRole(builder: TransactionBuilder) {
+    builder.createRole(
+        "whitelist_setter",
+        listOf(
+            Primitive.RolePermission.can_set_detail
+        )
     )
 }
 
-fun createRollBackRole(builder:GenesisBlockBuilder) {
-    builder.addTransaction(
-        Transaction.builder(null)
-            // create default "user" role
-            .createRole(
-                "rollback",
-                listOf(
-                    Primitive.RolePermission.can_transfer
-                )
-            ).build()
-            .build()
+fun createRollBackRole(builder: TransactionBuilder) {
+    builder.createRole(
+        "rollback",
+        listOf(
+            Primitive.RolePermission.can_transfer
+        )
     )
 }
 
-fun createNotaryListHolderRole(builder:GenesisBlockBuilder) {
-    builder.addTransaction(
-        Transaction.builder(null)
-            // create default "user" role
-            .createRole(
-                "notary_list_holder",
-                listOf(
-                    Primitive.RolePermission.can_set_detail
-                )
-            ).build()
-            .build()
+fun createNotaryListHolderRole(builder: TransactionBuilder) {
+    builder.createRole(
+        "notary_list_holder",
+        listOf(
+            Primitive.RolePermission.can_set_detail
+        )
     )
 }
 
-fun createSoraRole(builder:GenesisBlockBuilder) {
-    builder.addTransaction(
-        Transaction.builder(null)
-            // create default "user" role
-            .createRole(
-                "notary_list_holder",
-                listOf(
-                    Primitive.RolePermission.can_get_my_acc_ast,
-                    Primitive.RolePermission.can_transfer,
-                    Primitive.RolePermission.can_receive
-                )
-            ).build()
-            .build()
+fun createSoraRole(builder: TransactionBuilder) {
+    builder.createRole(
+        "notary_list_holder",
+        listOf(
+            Primitive.RolePermission.can_get_my_acc_ast,
+            Primitive.RolePermission.can_transfer,
+            Primitive.RolePermission.can_receive
+        )
     )
 }
 
-fun createSoraClientRole(builder:GenesisBlockBuilder) {
-    builder.addTransaction(
-        Transaction.builder(null)
-            // create default "user" role
-            .createRole(
-                "notary_list_holder",
-                listOf(
-                    Primitive.RolePermission.can_get_my_account,
-                    Primitive.RolePermission.can_get_my_acc_ast,
-                    Primitive.RolePermission.can_get_my_acc_ast_txs,
-                    Primitive.RolePermission.can_get_my_acc_txs,
-                    Primitive.RolePermission.can_get_my_txs,
-                    Primitive.RolePermission.can_transfer,
-                    Primitive.RolePermission.can_receive,
-                    Primitive.RolePermission.can_set_quorum,
-                    Primitive.RolePermission.can_add_signatory,
-                    Primitive.RolePermission.can_get_my_signatories,
-                    Primitive.RolePermission.can_remove_signatory
-                )
-            ).build()
-            .build()
+fun createSoraClientRole(builder: TransactionBuilder) {
+    builder.createRole(
+        "notary_list_holder",
+        listOf(
+            Primitive.RolePermission.can_get_my_account,
+            Primitive.RolePermission.can_get_my_acc_ast,
+            Primitive.RolePermission.can_get_my_acc_ast_txs,
+            Primitive.RolePermission.can_get_my_acc_txs,
+            Primitive.RolePermission.can_get_my_txs,
+            Primitive.RolePermission.can_transfer,
+            Primitive.RolePermission.can_receive,
+            Primitive.RolePermission.can_set_quorum,
+            Primitive.RolePermission.can_add_signatory,
+            Primitive.RolePermission.can_get_my_signatories,
+            Primitive.RolePermission.can_remove_signatory
+        )
     )
 }
 
-fun createBtcFeeRateSetterRole(builder:GenesisBlockBuilder) {
-    builder.addTransaction(
-        Transaction.builder(null)
-            // create default "user" role
-            .createRole(
-                "notary_list_holder",
-                listOf(
-                    Primitive.RolePermission.can_set_detail,
-                    Primitive.RolePermission.can_get_all_accounts
-                )
-            ).build()
-            .build()
+fun createBtcFeeRateSetterRole(builder: TransactionBuilder) {
+    builder.createRole(
+        "notary_list_holder",
+        listOf(
+            Primitive.RolePermission.can_set_detail,
+            Primitive.RolePermission.can_get_all_accounts
+        )
     )
 }
